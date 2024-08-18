00986acb821353560dcad28cb377b7e6a422246d
ssamko0911 <ssamko091184@gmail.com>
2024-08-18 20:08:08 +0100

    Refactor task 009 - l6

--- src/task009/task009.php
+++ src/task009/task009.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 //https://www.codewars.com/kata/56af1a20509ce5b9b000001e/train/php
 
+const ZIP_CODE_LENGTH = 8;
+
 function travel(string $address, string $zipcode): string
 {
-    define("ZIP_CODE_LENGTH", 8);
     $result = "$zipcode:/";
 
     $addressList = explode(',', $address);
@@ -14,7 +15,7 @@ function travel(string $address, string $zipcode): string
     $streetNames = [];
 
     foreach ($addressList as $address) {
-        if (strpos($address, $zipcode) && strlen($zipcode) === ZIP_CODE_LENGTH) {
+        if (str_contains($address, $zipcode) && strlen($zipcode) === ZIP_CODE_LENGTH) {
             $address = str_replace($zipcode, '', $address);
             $streetNumber = preg_replace("/[^0-9]/", '', $address);
             $streetNumbers[] = $streetNumber;
