c75573e34ba7d66d71d5a88dbec5cd19995d983c
ssamko0911 <ssamko091184@gmail.com>
2024-08-21 21:19:10 +0100

    Refactor task017

--- src/task017/script017.sql
+++ src/task017/script017.sql
@@ -13,6 +13,8 @@ values (4),
        (6),
        (0);
 
-SELECT numbers.number as num,
-       IF(numbers.number % 2 = 0, 'Even', 'Odd')
+SELECT numbers.number,
+       case when numbers.number % 2 = 0 then 'Even'
+       else 'Odd' end
+       AS is_even
 from numbers;
