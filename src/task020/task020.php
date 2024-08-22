fb19a476b08d2457ab409fb9e3fa321485b358d9
ssamko0911 <ssamko091184@gmail.com>
2024-08-22 12:23:46 +0100

    WIP

---
+++ src/task020/task020.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+//https://www.codewars.com/kata/57fb44a12b53146fe1000136/php
+
+function balance(string $l, string $r): string {
+    // Coding and coding ...
+}
