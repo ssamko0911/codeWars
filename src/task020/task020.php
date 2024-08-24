0fc10bdd5ea833da0a30ffb821560bcbd815260e
ssamko0911 <ssamko091184@gmail.com>
2024-08-24 22:27:45 +0100

    Add task Exclamation marks series - are they balanced - l6

--- src/task020/task020.php
+++ src/task020/task020.php
@@ -4,6 +4,38 @@ declare(strict_types=1);
 
 //https://www.codewars.com/kata/57fb44a12b53146fe1000136/php
 
-function balance(string $l, string $r): string {
-    // Coding and coding ...
+const SCORES = [
+    '!' => 2,
+    '?' => 3,
+];
+
+function balance(string $l, string $r): string
+{
+    $scoreLeft = getScore($l);
+    $scoreRight = getScore($r);
+
+    if ($scoreLeft > $scoreRight) {
+        $balanced = 'Left';
+    } elseif ($scoreLeft < $scoreRight) {
+        $balanced = 'Right';
+    } else {
+        $balanced = 'Balance';
+    }
+
+    return $balanced;
 }
+
+function getScore(string $str): int
+{
+    $score = 0;
+
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (array_key_exists($str[$i], SCORES)) {
+            $score += SCORES[$str[$i]];
+        }
+    }
+
+    return $score;
+}
+
+echo balance("!!???!????", "??!!?!!!!!!!");
